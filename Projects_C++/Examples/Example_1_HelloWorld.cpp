21c36a54d115c80b53a4582cbd7202fa3df3036b
FrJim7 <FrJim7@gmail.com>
2024-04-30 11:25:37 +0200

    Creation of a basic web using html

--- Projects_C++/Examples/Example_1_HelloWorld.cpp
+++ Projects_C++/Examples/Example_1_HelloWorld.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <iostream>
+
+
+int ParseRoute(char* src) {
+  char buffer[256];
+  sprintf(buffer, src);
+
+  std::cout << buffer;
+
+  return 0;
+}
 
 int main (int argc, char** argcv) {
-  printf("%s", argcv);
+  ParseRoute("Hello World from a function");
 
   return 0;
 }
